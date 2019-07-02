ea5b4031dfe3dd6e8a689dd9a19c52eac1281c57
Silvio Ap Silva | Kanazuchi <contato@kanazuchi.com>
2019-07-02 18:49:17 -0300

    Added first commit

--- sms_post.py
+++ sms_post.py
@@ -64,4 +64,4 @@ def smspost():
     return jsonify({"Status": "Send"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8081, debug=True)
+    app.run(host="0.0.0.0", port=8081, debug=False)
